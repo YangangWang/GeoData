fbaac95d6a7f7f403883c027a744acd615ce35d9
wangyangang <yangangwang1991@gmail.com>
2016-07-14 18:47:29 +0800

    .

--- AppStart/Form1.Designer.cs
+++ AppStart/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace testWinform2
+﻿namespace AppStart
 {
     partial class Form1
     {
@@ -23,29 +23,30 @@
         #region Windows 窗体设计器生成的代码
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要
+        /// 设计器支持所需的方法 - 不要修改
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent()
         {
-            this.navTreeControl1 = new DataInspectation.Controls.NavTreeControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // navTreeControl1
+            // button1
             // 
-            this.navTreeControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.navTreeControl1.Location = new System.Drawing.Point(0, 0);
-            this.navTreeControl1.Name = "navTreeControl1";
-            this.navTreeControl1.Size = new System.Drawing.Size(745, 565);
-            this.navTreeControl1.TabIndex = 0;
-            this.navTreeControl1.TotalWells = null;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(745, 565);
-            this.Controls.Add(this.navTreeControl1);
+            this.ClientSize = new System.Drawing.Size(748, 565);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -55,7 +56,8 @@
 
         #endregion
 
-        private DataInspectation.Controls.NavTreeControl navTreeControl1;
+        private System.Windows.Forms.Button button1;
+
     }
 }
 
---
+++ AppStart/Form1.cs
@@ -0,0 +1,44 @@
+﻿using DataInspectation.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppStart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        private DataTable GetTable()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("BZJH");
+            dt.Rows.Add("wang");
+            dt.Rows.Add("yan");
+
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NavTreeControl navtree = new NavTreeControl();
+            navtree.Dock = DockStyle.Fill;
+            navtree.TotalWells = GetTable();
+            Controls.Add(navtree);
+        }
+    }
+}
